6487c50f7bf3db7cb6e7ca755ba5d2ed2b216e4a
Akshay-bl <lattheakshay02@gmail>
2024-02-02 15:47:07 +0530

    terraform code 1st commit for ec2

---
+++ ec2.tf
@@ -0,0 +1,40 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = var.demo-sg-name
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {  
+    Name = "allow_tls"
+  }
+}
+
+
+resource "aws_instance" "Bastion-Host" {
+  count                  = 1
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = "demo-key" ## aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  tags = {
+    Name = "demo-Terraform"
+  }
+}
---
+++ vaiables.tf
@@ -0,0 +1,13 @@
+variable "demo-sg-name" {
+      type = string
+      
+}
+
+variable "ami_id" {
+  type = string
+      
+}
+variable "instance_type" {
+  type = string
+      
+}
