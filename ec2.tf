b355201e390fc85a129407ef3851b1070c263df8
Akshay-bl <lattheakshay02@gmail>
2024-02-05 12:33:38 +0530

    this is demo for the SG update for 22 to 8080

--- ec2.tf
+++ ec2.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "demo-sg" {
   ingress {
     description      = "SSH from VPC"
     from_port        = 22
-    to_port          = 22
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
